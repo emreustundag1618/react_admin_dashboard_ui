c59b31f50fd7d10cecf2482021a1b97491282fd9
emreustundag1618 <emre.ustundag.1.618@gmail.com>
2023-08-18 16:29:32 +0300

    Some files editted

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,24 @@ import "./navbar.scss";
 const Navbar = () => {
   return (
     <div className="navbar">
-      Navbar
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>Admin Panel</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon"/>
+        <img src="/app.svg" alt="" className="icon"/>
+        <img src="/expand.svg" alt="" className="icon"/>
+        <div className="notifications">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?h=34&w=34&fit=crop&crop=focalpoint&dpr=1" alt="" />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon"/>
+      </div>
     </div>
   )
 }
